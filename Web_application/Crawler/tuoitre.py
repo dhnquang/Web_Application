d78f41429e9d5164f0aab9d6d916f9bc4029aab4
dhnquang <59632967+dhnquang@users.noreply.github.com>
2020-12-02 21:31:52 +0700

    fix

--- Web_application/Crawler/tuoitre.py
+++ Web_application/Crawler/tuoitre.py
@@ -1,31 +1,33 @@
 import requests
-from bs4 import BeautifulSoup
+import bs4
 import pandas
 
+titles = []
 imgCaps = []
 imgLinks = []
 
-def crawl(image):
-    imgCaps.append(image.get('alt'))
-    imgLinks.append(image.get('src'))
 
-for i in range(1,41):
+for i in range(1,30):
     page = requests.get("https://tuoitre.vn/suc-khoe/phong-mach/trang-"+str(i)+".htm")
-    soup = BeautifulSoup(page.content,"html.parser")
+    soup = bs4.BeautifulSoup(page.content,"html.parser")
 
-    if (i == 1):
-        img = soup.find('img',{'class':'img324x202'})
-        crawl(img)
-        img = soup.find_all('img',{'class':'img124x77'})
-        for j in img:
-            crawl(j)
-    img = soup.find_all('img',{'class':'img212x132'})
-    for j in img:
-        crawl(j)
+    atribute = soup.find_all("li",{"class":"news-item"})
+    for j in atribute:
+        title = j.find('a')
+        img = j.find('img')
+        titles.append(title.get('title'))
+        imgLinks.append(img.get('src'))
+        imgCaps.append(img.get('alt'))
+
+print('\n'.join(titles))
+print('\n')
+print('\n'.join(imgCaps))
+print('\n')
+print('\n'.join(imgLinks))
 
 data = {
-    "Image Caption": imgCaps,
-    "Image URL": imgLinks
+   "Image Caption": imgCaps,
+   "Image URL": imgLinks
 }
 df = pandas.DataFrame(data)
-df.to_csv(r'..\UI_demo_II\Tuoitre.csv', index = False)
+df.to_csv(r'..\UI_demo_II\Tuoitre.csv', index=False)
